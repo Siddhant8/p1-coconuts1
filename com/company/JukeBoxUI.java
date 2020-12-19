8b6104e4af590b35af6574d31d5dbe0a0fe42a85
Siddhant8 <71453341+Siddhant8@users.noreply.github.com>
2020-12-18 16:35:41 -0800

    code is up again

--- com/company/JukeBoxUI.java
+++ com/company/JukeBoxUI.java
@@ -4,7 +4,44 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.*;
 
-public class JukeBoxUI {
+//Remember, this is for VIEW
+public class JukeBoxUI extends JFrame {
+
+    Image image;
+
+    public JukeBoxUI(){
+        getContentPane().setBackground(Color.BLUE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 500, 500);
+        getContentPane().setLayout(null);
+
+        image = new ImageIcon("src/jukebox.jpg").getImage();
+
+        setContentPane(new DrawPane());
+
+        JButton playMusic = new JButton("Click me to play the music");
+        playMusic.setBounds(0, 0, 150, 50);
+
+
+        getContentPane().add(playMusic);
+
+        setVisible(true);
+    }
+
+    class DrawPane extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+
+            //add draw image to paint
+            g.drawImage(image,0, 0, null);
+
+
+        }
+    }
+
+    public static void main(String args[]) { }
 
 
 }
+
--- com/company/TeaShop.java
+++ com/company/TeaShop.java
@@ -1,4 +1,34 @@
 package com.company;
 
-public class TeaShop {
+import javax.swing.*;
+import java.awt.*;
+
+public class TeaShop{
+    public TeaShop(){
+        JFrame menu = new JFrame("Home");
+
+        menu.setBounds(100, 100, 418, 315);
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel greeting = new JLabel("What tea Would You Like To Purchase?");
+
+
+        FlowLayout flow = new FlowLayout(); // Create a layout manager
+        flow.setHgap(10);                   // Set the horizontal gap
+        flow.setVgap(10);
+        flow.setAlignment(FlowLayout.CENTER);
+        Container content = menu.getContentPane(); // Get the content pane
+        content.setLayout(new GridLayout(2,2,10,10));
+
+        content.setLayout(flow); // Set the container layout mgr
+
+
+        content.add(greeting);
+
+        menu.setVisible(true);
+    }
+    public static void main(String args[]){
+        TeaShop frame = new TeaShop();
+    }
 }
+
